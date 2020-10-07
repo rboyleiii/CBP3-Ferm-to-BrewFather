d551e8c54ffb7539f920f3c6cba1500f5bf8cc43
RB3 <54190627+rboyleiii@users.noreply.github.com>
2020-10-06 20:01:25 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,29 +3,29 @@ import requests
 
 bf_uri = "http://log.brewfather.net/stream"
 
-def bf_api_id():
-  api_id = cbpi.get_config_parameter("brewfather_api_id", None)
+def bf_stream_id ():
+  api_id = cbpi.get_config_parameter("brewfather_stream_id", None)
   if api_id is None:
     try:
-      cbpi.add_config_parameter("brewfather_api_id", "", "text", "Brewfather API Id")
+      cbpi.add_config_parameter("brewfather_stream_id", "", "text", "Brewfather stream API ID")
       return ""
     except:
-      cbpi.notify("Brewfather Error", "Unable to update brewfather_api_id parameter within database. Try updating CraftBeerPi and reboot.", type="danger", timeout=None)
+      cbpi.notify("Brewfather Error", "Unable to update brewfather_stream_id parameter within database. Try updating CraftBeerPi and reboot.", type="danger", timeout=None)
   else:
     return api_id
 
 
 @cbpi.backgroundtask(key="brewfather_task", interval=900)
 def brewfather_background_task(api):
-  api_id = bf_api_id()
+  api_id = bf_stream_id()
   if api_id == "":
-    cbpi.notify("Brewfather Error", "Id not set. Update brewfather_api_id parameter within System > Parameters.", type="danger", timeout=None)
+    cbpi.notify("Brewfather Error", "Stream ID not set. Update brewfather_stream_id parameter within System > Parameters.", type="danger", timeout=None)
     return
 
   for i, fermenter in cbpi.cache.get("fermenter").iteritems():
 
-    # if we have a beer name, we will log the temperatures
-    if fermenter.brewname and fermenter.brewname.strip():
+    # if fermenter is active, we will log the temperatures
+    if fermenter.state is not False:
       try:
         queryString = {
           "id": api_id
